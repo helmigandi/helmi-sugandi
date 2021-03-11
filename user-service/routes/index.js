079c4d3166bf1f98c57925a5323a25cc1276a297
Helmi Sugandi <sugandihelmi@gmail.com>
2021-03-11 08:13:08 +0700

    remove generate token and console log

--- user-service/routes/index.js
+++ user-service/routes/index.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 const userRoutes = require("./user");
-const TokenController = require("../controllers/TokenController");
 const { authorization } = require("../middlewares/auth");
 
 router.get("/", (req, res) => {
   return res.status(200).json({ message: "Home User-Service API" });
 });
 
-router.get("/getToken", TokenController.generateToken);
 router.use(authorization);
 router.use("/users", userRoutes);
 
